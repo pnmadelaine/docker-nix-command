42c89333c788aa5f4afb566ce2d8d086bfc86f58
Paul-Nicolas Madelaine <pnm@pnm.tf>
2024-06-13 10:42:39 +0200

    fourth commit

--- nix.sh
+++ nix.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm --volume $(pwd):/mnt nixos/nix bash -c "git config --system --add safe.directory /mnt && cd /mnt && HOME=/home nix --extra-experimental-features 'nix-command flakes' $*"
+docker run --rm --volume $(pwd):/mnt -it nixos/nix bash -c "git config --system --add safe.directory /mnt && cd /mnt && HOME=/home nix --extra-experimental-features 'nix-command flakes' $*"
